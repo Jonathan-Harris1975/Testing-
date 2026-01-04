848fb44aff07f5195be531aeee4cbfaaf698d20c
Jonathan-Harris1975 <johnbristol26@gmail.com>
2026-01-04 22:19:06 +0000

    Add files via upload

--- services/rss-feed-creator/startup/rss-init.js
+++ services/rss-feed-creator/startup/rss-init.js
@@ -1,9 +1,13 @@
-// services/rss-feed-creator/startup/rss-init.js
-import { ensureR2Sources } from "../utils/rss-bootstrap.js";
 import { info } from "../../../logger.js";
+import { ensureBucketKey } from "../../shared/utils/r2-client.js";
 
-(async () => {
+export async function ensureR2Sources() {
+  ensureBucketKey("rss");
+  ensureBucketKey("podcastRss");
+  info("🟩 RSS bootstrap complete.");
+}
+
+if (process.argv[1]?.includes("rss-init.js")) {
   info("🧩 RSS bootstrap starting...");
   await ensureR2Sources();
-  info("🟩 RSS bootstrap complete.");
-})();
+}
