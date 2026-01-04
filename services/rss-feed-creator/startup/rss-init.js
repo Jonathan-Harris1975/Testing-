024269721650df3aa5d73fec5b5f5b606918aa81
Jonathan-Harris1975 <johnbristol26@gmail.com>
2026-01-04 17:09:55 +0000

    Add files via upload

--- services/rss-feed-creator/startup/rss-init.js
+++ services/rss-feed-creator/startup/rss-init.js
@@ -1,6 +1,6 @@
 // services/rss-feed-creator/startup/rss-init.js
 import { ensureR2Sources } from "../utils/rss-bootstrap.js";
-import { info } from "../../shared/logger.js";
+import { info } from "../../../logger.js";
 
 (async () => {
   info("🧩 RSS bootstrap starting...");
